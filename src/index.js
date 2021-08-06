959f3acf83dd73a73a77a8c52fa6ada8828906a0
Robbie-McGregor <40312007+Robbie-McGregor@users.noreply.github.com>
2021-08-06 18:45:10 +1200

    Section Finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Pizza</li>
+      <li>Ice Cream</li>
+    </ul>
+  </>,
+  document.getElementById("root")
+);
+
 //Create a react app from scratch.
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
